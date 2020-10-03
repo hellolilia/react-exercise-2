3881c3ce534a214806be6a49b9ff081a70e08cba
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-03 21:18:33 +0800

    add cart

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import cartImage from '../assets/cart_image.png';
+import '../styles/Header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="storeTitle">
+        <h1>Store</h1>
+        <img src={cartImage} alt={cartImage} />
+      </div>
+    );
+  }
+}
+
+export default Header;
